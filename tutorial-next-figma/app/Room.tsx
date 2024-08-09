f604bc9ea7e6d565efaac191787dfcc80e56862a
Jack Maders <jackwmaders@gmail.com>
2024-08-09 16:29:17 +0100

    feat: :sparkles: init liveblocks

---
+++ tutorial-next-figma/app/Room.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+  if (!process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY) {
+    throw new Error(
+      "Missing environment variable: NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY"
+    );
+  }
+
+  return (
+    <LiveblocksProvider
+      publicApiKey={process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY}
+    >
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
